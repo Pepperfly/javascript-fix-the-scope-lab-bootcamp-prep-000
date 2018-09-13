79ab273d8b545a1595a358bf185970ef88779a6b
Jeff Carter <jeff@pepperfly.com>
2018-09-13 20:47:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,6 +29,14 @@ var funkyFunction = function() {
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 
+// Jeff says:
+// This works in other code sandboxes. Why does it produce an error here?
+// var theFunk = funkyFunction();
+
+// Jeff says:
+// The following was an accident, yet I found that it passes its test.
+// The following code produces and error in other code sandboxes.
 var theFunk = funkyFunction()();
+// What's going on?
 
 //
